108dc7a24f60b7b24d7e214174e60a9b844f3b86
Mateusz Łokietek <mateusz.lokietek.001@gmail.com>
2022-03-30 19:14:21 +0200

    Added md4 function implementation

--- src/hashing.h
+++ src/hashing.h
@@ -7,7 +7,7 @@ struct hashing {
     uint32_t* (*sha_1)(uint8_t*,uint64_t);
     uint32_t* (*sha_0)(uint8_t*,uint64_t);
     uint32_t* (*md_5)(uint8_t*,uint64_t);
-    const char* (*md_4)(const char*);
+    uint32_t* (*md_4)(uint8_t*,uint64_t);
     const char* (*rot_13)(const char*);
 };
 
