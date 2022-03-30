980c4fb078c5863cc8a82591c6ea23bf199dd0f2
Mateusz Łokietek <mateusz.lokietek.001@gmail.com>
2022-03-30 21:14:30 +0200

    Added rot13 function implementation

--- src/hashing.h
+++ src/hashing.h
@@ -8,7 +8,7 @@ struct hashing {
     uint32_t* (*sha_0)(uint8_t*,uint64_t);
     uint32_t* (*md_5)(uint8_t*,uint64_t);
     uint32_t* (*md_4)(uint8_t*,uint64_t);
-    const char* (*rot_13)(const char*);
+    uint8_t* (*rot_13)(uint8_t*,uint64_t);
 };
 
 extern const struct hashing Hashing;
